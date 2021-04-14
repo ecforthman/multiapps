2eccd71eb55a0c69024ecee22e0cdf8000438946
George <ezen.1.618@gmail.com>
2021-04-14 15:29:03 -0400

    move .env

--- multiapps_project/settings.py
+++ multiapps_project/settings.py
@@ -16,9 +16,9 @@ import dj_database_url
 import environ
 
 env = environ.Env()
-environ.Env.read_env(env_file='.env')
+environ.Env.read_env()
 
-
+# env_file='../.env'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
